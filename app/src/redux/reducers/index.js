685900e4d52adb7ac44f92fe3c5379a28b08f52e
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-07-04 17:14:28 +0200

    #1_set up redux and installed dependancies

---
+++ app/src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+//**import all reducers here**
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    // all reducers will be here named as follows:
+    // name: nameReducer
+    // isLogged: loggedReducer
+})
+
+export default rootReducer;
