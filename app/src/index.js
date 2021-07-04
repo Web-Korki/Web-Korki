203747138a6e900eefff535c5a9d64bc5205508e
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-07-04 17:20:46 +0200

    #1_redux dev tools added

--- app/src/index.js
+++ app/src/index.js
@@ -7,7 +7,7 @@ import rootReducer from './redux/reducers/index';
 
 const store = createStore(
   rootReducer,
-  window._REDUX_DEVTOOLS_EXTENSION_&&window._REDUX_DEVTOOLS_EXTENSION_()
+  window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
   );
 
 ReactDOM.render(
