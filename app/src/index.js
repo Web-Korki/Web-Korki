a827c4ec360e93e232fe1f33ffbda8d1888348ea
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-07-04 17:18:05 +0200

    #1_redux dev tools added

--- app/src/index.js
+++ app/src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import { createStore } from 'redux';
 import rootReducer from './redux/reducers/index';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window._REDUX_DEVTOOLS_EXTENSION_&&window._REDUX_DEVTOOLS_EXTENSION_()
+  );
 
 ReactDOM.render(
   <React.StrictMode>
