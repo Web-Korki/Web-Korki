8cdae2a1c4287afc4e595545e332ff052cb3fee8
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-07-10 23:32:45 +0200

    #1_mockUps-added-LectureAnalysis-container

--- app/src/App.js
+++ app/src/App.js
@@ -6,6 +6,7 @@ import { InactiveReplacement } from './core/inactiveReplacement/containers/Inact
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import { Route, Switch } from "react-router";
+import { LectureAnalysis } from "./core/lectureAnalysis/containers/LectureAnalysis";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           <Route exact path="/UserMenu" component={UserMenu} />
           <Route path='/login_form' component={LoginForm} />
           <Route path='/inactive_replacement' component={InactiveReplacement} />
-
+          <Route path='/lecture_analysis' component={LectureAnalysis} />
         </Switch>
 
       </MainContainer>
