07db3045be4e5efeac812086d35941650afcba25
Daniel <konnekdaniel@gmail.com>
2021-07-12 19:06:48 +0200

    #2_mockUps_Admin_menu_added_and_styles_update

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { MainContainer } from "./core/MainContainer";
-import { UserMenu } from "./core/userMenu/containers/UserMenu";
+import React from 'react';
+import { MainContainer } from './core/MainContainer';
+import { UserMenu } from './core/userMenu/containers/UserMenu';
+import { AdminMenu } from './core/adminMenu/containers/AdminMenu';
 import { LoginForm } from './core/loginForm/containers/LoginForm';
 import { InactiveReplacement } from './core/inactiveReplacement/containers/InactiveReplacement';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import { Route, Switch } from "react-router";
-import { LectureAnalysis } from "./core/lectureAnalysis/containers/LectureAnalysis";
+import { Route, Switch } from 'react-router';
+import { LectureAnalysis } from './core/lectureAnalysis/containers/LectureAnalysis';
 
 function App() {
   return (
     <>
       <MainContainer>
-
         <Switch>
           <Route exact path="/UserMenu" component={UserMenu} />
-          <Route path='/login_form' component={LoginForm} />
-          <Route path='/inactive_replacement' component={InactiveReplacement} />
-          <Route path='/lecture_analysis' component={LectureAnalysis} />
+          <Route path="/AdminMenu" component={AdminMenu} />
+          <Route path="/login_form" component={LoginForm} />
+          <Route path="/inactive_replacement" component={InactiveReplacement} />
+          <Route path="/lecture_analysis" component={LectureAnalysis} />
         </Switch>
-
       </MainContainer>
     </>
   );
