323fb9b0c1c753a5878709db87224cfb98b0ca88
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-07-31 22:12:34 +0200

    fix routing

--- app/src/core/loginForm/styledComponents/StyledInput.js
+++ app/src/core/loginForm/styledComponents/StyledInput.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const StyledInput = styled.input`
-	width: 309px;
-	height: 50px;
+	width: clamp(173px, 300px, 309px);
+	height: clamp(40px, 2.6vw, 50px);
 	background: #ffffff;
 	border: 1px solid #ebeff1;
 	box-shadow: 1px 1px 5px rgba(55, 55, 55, 0.09);
@@ -24,6 +24,12 @@ const StyledInput = styled.input`
 	&:focus {
 		outline: none;
 		box-shadow: 1px 1px 5px rgba(55, 55, 55, 0.3);
+	}
+
+	/* Galaxy Fold responsivness - just because I can xd */
+	@media (width: 280px) {
+		width: 240px;
+		height: 40px;
 	}
 `;
 
