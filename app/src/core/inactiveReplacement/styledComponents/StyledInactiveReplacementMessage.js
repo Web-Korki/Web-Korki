eba3e4ea1b0c5193dad306d7474619671b24a8de
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-08-01 15:39:52 +0200

    #4_inactiveReplacement-fix mobile styles

--- app/src/core/inactiveReplacement/styledComponents/StyledInactiveReplacementMessage.js
+++ app/src/core/inactiveReplacement/styledComponents/StyledInactiveReplacementMessage.js
@@ -1,18 +1,36 @@
 import styled from 'styled-components';
 
 const StyledInactiveReplacementMessage = styled.button`
-    width: 100%;
-    height: 200px;
-    background-color:#FBFCFD;
-    border: none;
-    border-radius: 10px;
-    box-shadow: 2px 2px 5px 2px #CCE4F4;
-    padding: 20px;
-    margin-top: 50px;
-    color: #195669;
+	width: 100%;
+	height: 200px;
+	background-color: #fbfcfd;
+	border: none;
+	border-radius: 10px;
+	box-shadow: 2px 2px 5px 2px #cce4f4;
+	padding: 20px;
+	margin-top: 50px;
+	color: #195669;
 
-    &:hover{
-        background-color: #EBEFF1;
-    }
-`
+	@media (hover: hover) {
+		&:hover {
+			background-color: #ebeff1;
+		}
+	}
+
+	@media (max-width: 414px) {
+		.inactiveReplacementWrapper {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+		}
+
+		width: 334px;
+		height: 135px;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -20%);
+	}
+`;
 export default StyledInactiveReplacementMessage
