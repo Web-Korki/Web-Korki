d10bb30b6840115d104009019c47dcbd49f67d51
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-08-01 16:38:06 +0200

    #4_lectureAnalysis-fix-mobile-styles for tablet

--- app/src/core/lectureAnalysis/containers/LectureAnalysis.jsx
+++ app/src/core/lectureAnalysis/containers/LectureAnalysis.jsx
@@ -3,54 +3,54 @@ import { StyledBoxLectureAnalysis } from '../styledComponents/index';
 
 export const LectureAnalysis = () => {
   return (
-    <>
-      <div className="lectureAnalysisWrapper d-flex align-items-center justify-content-center">
-        <div className="lectureAnalysisInnerWrapper">
-          <div className="row">
-            <div className="lectureAnalysisTitle text-center col-12">
-              <h1>Analiza - zajęcia</h1>
-            </div>
-          </div>
-          <div className="row mt-5">
-            <div className="col p-0">
-              <StyledBoxLectureAnalysis>Wrzesień</StyledBoxLectureAnalysis>
-            </div>
-            <div className="col p-0">
-              <StyledBoxLectureAnalysis>Październik</StyledBoxLectureAnalysis>
-            </div>
-            <div className="col p-0">
-              <StyledBoxLectureAnalysis>Listopad</StyledBoxLectureAnalysis>
-            </div>
-            <div className="col p-0">
-              <StyledBoxLectureAnalysis>Grudzień</StyledBoxLectureAnalysis>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col p-0">
-              <StyledBoxLectureAnalysis>Styczeń</StyledBoxLectureAnalysis>
-            </div>
-            <div className="col p-0">
-              <StyledBoxLectureAnalysis>Luty</StyledBoxLectureAnalysis>
-            </div>
-            <div className="col p-0">
-              <StyledBoxLectureAnalysis>Marzec</StyledBoxLectureAnalysis>
-            </div>
-            <div className="col p-0">
-              <StyledBoxLectureAnalysis>Kwiecień</StyledBoxLectureAnalysis>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col"></div>
-            <div className="col p-0">
-              <StyledBoxLectureAnalysis>Maj</StyledBoxLectureAnalysis>
-            </div>
-            <div className="col p-0">
-              <StyledBoxLectureAnalysis>Czerwiec</StyledBoxLectureAnalysis>
-            </div>
-            <div className="col"></div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+		<>
+			<div className='lectureAnalysisWrapper d-flex align-items-center justify-content-center'>
+				<div className='lectureAnalysisInnerWrapper'>
+					<div className='row'>
+						<div className='lectureAnalysisTitle text-center col-12'>
+							<h1>Analiza - zajęcia</h1>
+						</div>
+					</div>
+					<div className='row mt-5 m-md-2'>
+						<div className='col-lg-3 col-md-6 col-sm-12 p-0'>
+							<StyledBoxLectureAnalysis>Wrzesień</StyledBoxLectureAnalysis>
+						</div>
+						<div className='col-lg-3 col-md-6 col-sm-12 p-0'>
+							<StyledBoxLectureAnalysis>Październik</StyledBoxLectureAnalysis>
+						</div>
+						<div className='col-lg-3 col-md-6 col-sm-12 p-0'>
+							<StyledBoxLectureAnalysis>Listopad</StyledBoxLectureAnalysis>
+						</div>
+						<div className='col-lg-3 col-md-6 col-sm-12 p-0'>
+							<StyledBoxLectureAnalysis>Grudzień</StyledBoxLectureAnalysis>
+						</div>
+					</div>
+					<div className='row m-md-2'>
+						<div className='col-lg-3 col-md-6 col-sm-12 p-0'>
+							<StyledBoxLectureAnalysis>Styczeń</StyledBoxLectureAnalysis>
+						</div>
+						<div className='col-lg-3 col-md-6 col-sm-12 p-0'>
+							<StyledBoxLectureAnalysis>Luty</StyledBoxLectureAnalysis>
+						</div>
+						<div className='col-lg-3 col-md-6 col-sm-12 p-0'>
+							<StyledBoxLectureAnalysis>Marzec</StyledBoxLectureAnalysis>
+						</div>
+						<div className='col-lg-3 col-md-6 col-sm-12 p-0'>
+							<StyledBoxLectureAnalysis>Kwiecień</StyledBoxLectureAnalysis>
+						</div>
+					</div>
+					<div className='row m-md-2'>
+						<div className='col-lg-3 col-md-0 col-sm-12 p-0'></div>
+						<div className='col-lg-3 col-md-6 col-sm-12 p-0'>
+							<StyledBoxLectureAnalysis>Maj</StyledBoxLectureAnalysis>
+						</div>
+						<div className='col-lg-3 col-md-6 col-sm-12 p-0'>
+							<StyledBoxLectureAnalysis>Czerwiec</StyledBoxLectureAnalysis>
+						</div>
+						<div className='col-lg-3 col-md-0 col-sm-12 p-0'></div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
