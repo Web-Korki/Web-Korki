4d9dd195566fb7c132a00ac5677f52582fbbcb53
Daniel <konnekdaniel@gmail.com>
2021-08-08 10:05:44 +0200

    add missing file

---
+++ app/src/core/sharedComponents/styledComponents/StyledBoxMonths.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const StyledBoxMonths = styled.button`
+  padding: 40px;
+  box-sizing: border-box;
+  min-width: 240px;
+  min-height: 120px;
+  font-size: 24px;
+  line-height: 29px;
+  margin: 10px;
+  background: #fbfcfd;
+  border: 2px solid #ebf3f8;
+  box-shadow: 4px 4px 15px 4px rgba(7, 93, 172, 0.12);
+  border-radius: 10px;
+  color: #195669;
+
+  &:hover {
+    background: #ebeff1;
+  }
+`;
+export default StyledBoxMonths;
