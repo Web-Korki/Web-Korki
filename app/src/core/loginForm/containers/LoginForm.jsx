536ba3b09f386d58d8e1b4d9055da3b64cad75f1
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-08-15 16:46:13 +0200

    merge

--- app/src/core/loginForm/containers/LoginForm.jsx
+++ app/src/core/loginForm/containers/LoginForm.jsx
@@ -1,23 +1,62 @@
 import React, { useState } from 'react';
-import content from '../../constans/loginFormIndex';
-import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 import { StyledLoginBox, StyledInput } from '../styledComponents/index';
 
 import { StyledBlueButton } from '../../sharedComponents/styledComponents';
 
 export const LoginForm = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// axios
+		// 	.post('http://nujgoiz.cluster024.hosting.ovh.net/token/obtain/', {
+		// 		username: email,
+		// 		password: password,
+		// 	})
+		// 	.then((res) => {
+		// 		console.log(res);
+		// 	})
+		// 	.catch((error) => {
+		// 		console.log(error);
+		// 	});
+		axios
+			.post('http://nujgoiz.cluster024.hosting.ovh.net/api/auth/login/', {
+				username: email,
+				password: password,
+			})
+			.then((res) => {
+				console.log(res);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	};
+
 	return (
 		<div className='d-flex justify-content-center align-items-center flex-column loginForm'>
 			<StyledLoginBox className='d-flex align-items-center flex-column'>
 				<h1 className='mt-md-5 mb-3'>Logowanie do platformy</h1>
 				<StyledInput
+					id='name'
 					type='text'
 					placeholder='login'
 					className='mt-md-5 mt-4 mb-4'
+					onChange={(e) => setEmail(e.target.value)}
 				/>
-				<StyledInput type='password' placeholder='hasło' />
-				<StyledBlueButton type='submit' className='mt-4 px-5 py-2'>
+				<StyledInput
+					id='password'
+					type='password'
+					placeholder='hasło'
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<StyledBlueButton
+					type='submit'
+					className='mt-4 px-5 py-2'
+					onClick={handleSubmit}>
 					zaloguj się
 				</StyledBlueButton>
 			</StyledLoginBox>
