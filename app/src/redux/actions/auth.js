0dfd05249f740f6c88bcadacd365e885d69d0580
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-09-20 20:25:43 +0200

    reducers-auth

--- app/src/redux/actions/auth.js
+++ app/src/redux/actions/auth.js
@@ -1,41 +1,41 @@
-import axios from 'axios';
-import { USER_LOADED, USER_LOADING, AUTH_ERROR } from './types';
+// import axios from 'axios';
+// import { LOGIN_FAIL, USER_LOADED, USER_LOADING } from './types';
 
-const baseURL = 'http://nujgoiz.cluster024.hosting.ovh.net';
-// CHECK TOKEN & LOAD USER:
-export const loadUser = () => (dispatch, getState) => {
-	//User loading:
-	dispatch({
-		type: USER_LOADING,
-	});
+// const baseURL = 'http://nujgoiz.cluster024.hosting.ovh.net';
+// // CHECK TOKEN & LOAD USER:
+// export const loadUser = () => (dispatch, getState) => {
+// 	//User loading:
+// 	dispatch({
+// 		type: USER_LOADING,
+// 	});
 
-	//Get token from state:
-	const token = getState().auth.token;
+// 	//Get token from state:
+// 	const token = getState().auth.token;
 
-	//Headers
-	const config = {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	};
+// 	//Headers
+// 	const config = {
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 		},
+// 	};
 
-	//If token, add to headers config
-	if (token) {
-		config.headers['Authorization'] = `Token ${token}`;
-	}
+// 	//If token, add to headers config
+// 	if (token) {
+// 		config.headers['Authorization'] = `Token ${token}`;
+// 	}
 
-	axios
-		.get(`${baseURL}/api/auth/user`, config)
-		.then((res) => {
-			dispatch({
-				type: USER_LOADED,
-				payload: res.data,
-			});
-		})
-		.catch((err) => {
-			console.log(err.response.data, err.response.status);
-			dispatch({
-				type: AUTH_ERROR,
-			});
-		});
-};
+// 	axios
+// 		.get(`${baseURL}/api/auth/user`, config)
+// 		.then((res) => {
+// 			dispatch({
+// 				type: USER_LOADED,
+// 				payload: res.data,
+// 			});
+// 		})
+// 		.catch((err) => {
+// 			console.log(err.response.data, err.response.status);
+// 			dispatch({
+// 				type: LOGIN_FAIL,
+// 			});
+// 		});
+// };
