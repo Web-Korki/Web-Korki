99de6763f5512a6bdeb4d45b32d759e77812b2c7
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-10-16 20:33:32 +0200

    fixed the issues with Alerts component export

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,22 @@
+//react
 import React, { useEffect } from 'react';
-import { Route, Switch } from 'react-router';
+
+//css
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
+//redux
+import store from './store';
+import { connect } from 'react-redux';
+import { checkAuthenticated, load_user } from './redux/actions/auth';
+
+//router
+import { Route, Switch } from 'react-router';
+
+//components
 //all styled components are in the same place now. Remember to add them to index.js in the directory src/components/styledComponents
+//named imports
 import { MainContainer } from './components/styledComponents/index.js';
-import UserMenu from './containers/menus/UserMenu';
-import AdminMenu from './containers/menus/AdminMenu';
-import LoginForm from './containers/forms/LoginForm';
-import RegisterForm from './containers/forms/RegisterForm';
 import { InactiveReplacement } from './containers/InactiveReplacement';
 import { LectureAnalysis } from './containers/lectureAnalysis/LectureAnalysis';
 import { LectureAnalysisDetail } from './containers//lectureAnalysis/LectureAnalysisDetail';
@@ -15,17 +24,16 @@ import { VolunteerAnalysis } from './containers/volunteerAnalysis/VolunteerAnaly
 import { VolunteerAnalysisDetail } from './containers/volunteerAnalysis/VolunteerAnalysisDetail';
 import { Whoops404 } from './containers/Whoops404';
 import { Home } from './containers/Home';
+//default imports
+import UserMenu from './containers/menus/UserMenu';
+import AdminMenu from './containers/menus/AdminMenu';
+import LoginForm from './containers/forms/LoginForm';
+import RegisterForm from './containers/forms/RegisterForm';
 import PrivateRoute from './containers/PrivateRoute';
-
-//API
-import store from './store';
-import { connect } from 'react-redux';
-import { checkAuthenticated, load_user } from './redux/actions/auth';
 import ActivateAccount from './containers/ActivateAccount';
 import ResetPassword from './containers/passwordReset/ResetPassword';
 import ResetPasswordConfirm from './containers/passwordReset/ResetPasswordConfirm';
 import SubmitReplacement from './containers/forms/SubmitReplacement';
-
 
 function App() {
 	useEffect(() => {
@@ -34,56 +42,56 @@ function App() {
 	});
 
 	return (
-      <>
-         <MainContainer>
-            <Switch>
-               <Route exact path='/' component={Home} />
-               <PrivateRoute exact path='/UserMenu' component={UserMenu} />
-               <PrivateRoute path='/admin_menu' component={AdminMenu} />
-               <Route path='/login_form' component={LoginForm} />
-               <PrivateRoute path='/register_form' component={RegisterForm} />
-               <PrivateRoute
-                  path='/inactive_replacement'
-                  component={InactiveReplacement}
-               />
-               <Route
-                  path='/submit_replacement'
-                  exact
-                  component={SubmitReplacement}
-               />
-               <Route
-                  path='/lecture_analysis'
-                  exact
-                  component={LectureAnalysis}
-               />
-               <Route
-                  path='/lecture_analysis/:month'
-                  component={LectureAnalysisDetail}
-               />
-               <Route
-                  path='/volunteer_analysis'
-                  exact
-                  component={VolunteerAnalysis}
-               />
-               <Route
-                  path='/volunteer_analysis/:month'
-                  component={VolunteerAnalysisDetail}
-               />
-               {/* <PrivateRoute path='/volunteer_register' component={VolunteerRegister} /> */}
-               <Route
-                  path='/activate/{uid}/{token}'
-                  component={ActivateAccount}
-               />
-               <Route path='/reset_password' component={ResetPassword} />
-               <Route
-                  path='/password/reset/confirm/{uid}/{token}'
-                  component={ResetPasswordConfirm}
-               />
-               <Route path='*' component={Whoops404} />
-            </Switch>
-         </MainContainer>
-      </>
-   );
+		<>
+			<MainContainer>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<PrivateRoute exact path='/UserMenu' component={UserMenu} />
+					<PrivateRoute path='/admin_menu' component={AdminMenu} />
+					<Route path='/login_form' component={LoginForm} />
+					<PrivateRoute path='/register_form' component={RegisterForm} />
+					<PrivateRoute
+						path='/inactive_replacement'
+						component={InactiveReplacement}
+					/>
+					<Route
+						path='/submit_replacement'
+						exact
+						component={SubmitReplacement}
+					/>
+					<Route
+						path='/lecture_analysis'
+						exact
+						component={LectureAnalysis}
+					/>
+					<Route
+						path='/lecture_analysis/:month'
+						component={LectureAnalysisDetail}
+					/>
+					<Route
+						path='/volunteer_analysis'
+						exact
+						component={VolunteerAnalysis}
+					/>
+					<Route
+						path='/volunteer_analysis/:month'
+						component={VolunteerAnalysisDetail}
+					/>
+					{/* <PrivateRoute path='/volunteer_register' component={VolunteerRegister} /> */}
+					<Route
+						path='/activate/{uid}/{token}'
+						component={ActivateAccount}
+					/>
+					<Route path='/reset_password' component={ResetPassword} />
+					<Route
+						path='/password/reset/confirm/{uid}/{token}'
+						component={ResetPasswordConfirm}
+					/>
+					<Route path='*' component={Whoops404} />
+				</Switch>
+			</MainContainer>
+		</>
+	);
 }
 
 export default connect(null, { checkAuthenticated, load_user })(App);
--- app/src/index.js
+++ app/src/index.js
@@ -1,19 +1,47 @@
-import React from 'react';
+//react
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
+//css
 import './index.css';
 import './reset.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from "./store"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+//router
+import { BrowserRouter as Router } from 'react-router-dom';
+
+//alerts
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+import Alerts from './containers/alerts/Alerts';
+
+//components
+import App from './App';
+
+//Alert Options:
+const alertOptions = {
+	timeout: 3000,
+	position: 'top center',
+};
+
+class Index extends Component {
+	render() {
+		return (
+			<React.StrictMode>
+				<Provider store={store}>
+					<AlertProvider template={AlertTemplate} {...alertOptions}>
+						<Alerts />
+						<Router>
+							<App />
+						</Router>
+					</AlertProvider>
+				</Provider>
+			</React.StrictMode>
+		);
+	}
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
