8a54c76ed435ba53bca527a387c295f6cce8747b
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-10-24 13:58:17 +0200

    merge with master

--- app/src/components/styledComponents/MainContainer.js
+++ app/src/components/styledComponents/MainContainer.js
@@ -1,14 +1,9 @@
 import styled from 'styled-components';
 
 const MainContainer = styled.div`
-<<<<<<< HEAD
-	margin: 0 auto;
-	max-width: 1024px;
-=======
   margin: 0 auto;
   max-width: 1024px;
   height: 100%;
->>>>>>> master
 `;
 
 export default MainContainer;
