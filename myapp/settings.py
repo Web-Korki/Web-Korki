222f01725f8ae09b65035803e589aa6cad3584c9
Tomasz Jastrzębski <65226136+hawkesky@users.noreply.github.com>
2021-10-26 21:02:00 +0200

    Settings fix

--- myapp/settings.py
+++ myapp/settings.py
@@ -158,6 +158,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, FRONTEND_DIR, "build", "static"),
+    os.path.join(BASE_DIR, FRONTEND_DIR, "build"),
 )
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
