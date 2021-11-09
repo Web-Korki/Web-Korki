56451ee590f4a862317bb44984dcfbf9e1373604
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-11-09 19:55:53 +0100

    disable devtools on prod plus correct propTypes in alerts.jsx

--- app/src/containers/PrivateRoute.js
+++ app/src/containers/PrivateRoute.js
@@ -4,7 +4,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 //router
 import { Route } from 'react-router-dom';
-import { Redirect } from 'react-router';
 //utils
 import Loader from 'react-loader-spinner';
 //proptypes
