1465857507e2a56c38c2294d100b3365cd82e2bf
Tomasz Jastrzębski <tomasz.jastrzebski95@gmail.com>
2021-11-16 20:11:26 +0100

    review corrections

--- backend/admin.py
+++ backend/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Teacher, Email, EmailFooter, Subject
+from .models import Teacher, Email, EmailFooter, Subject, Level
 
 UserAdmin.list_display = ("username", "email", "fb_name", "is_active", "is_staff")
 UserAdmin.fieldsets[1][1]["fields"] += ("fb_name",)
@@ -9,3 +9,5 @@ UserAdmin.fieldsets += ((("Przedmioty"), {"fields": ("subjects",)}),)
 admin.site.register(Teacher, UserAdmin)
 admin.site.register(Email)
 admin.site.register(EmailFooter)
+admin.site.register(Subject)
+admin.site.register(Level)
