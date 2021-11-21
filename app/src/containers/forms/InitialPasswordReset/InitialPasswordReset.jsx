a9d2c937df8fc5749208635b9ac2c71abf6c4fe7
Daniel <konnekdaniel@gmail.com>
2021-11-21 18:07:30 +0100

    layout changes/improvements on initialPasswordChange and reset_password

--- app/src/containers/forms/InitialPasswordReset/InitialPasswordReset.jsx
+++ app/src/containers/forms/InitialPasswordReset/InitialPasswordReset.jsx
@@ -102,28 +102,29 @@ const InitialPasswordReset = ({
   }
 
   return (
-    <div className="resetPasswordConfirm d-flex flex-column justify-content-center align-items-center h-100">
-      <Wrapper className="d-flex flex-column justify-content-center align-items-center">
-        <h1 className="title mb-4">Ustaw własne hasło</h1>
+    <div className="min-h-100 py-5 py-lg-0 d-flex flex-column justify-content-center align-items-center">
+      <Wrapper>
+        <h1 className="title mb-4">Formularz resetowania hasła</h1>
         <form
           className="d-flex align-items-center flex-column"
           onSubmit={(e) => onSubmit(e)}
         >
+          <p className="text">Wprowadź nazwę z facebook</p>
           <Input
             id="fb_name"
             type="text"
             className="mb-3 mb-md-4"
-            placeholder="twój login na facebooku"
+            placeholder="Adam Kowalski"
             name="fb_name"
             value={fb_name}
             onChange={(e) => onChange(e)}
             required
           />
-          <div className="position-relative">
+          <p className="text">Podaj nowe hasło</p>
+          <div className="position-relative mb-3 mb-md-4">
             <Input
               id="password"
               type={oldPasswordShown ? 'text' : 'password'}
-              className="mb-3 mb-md-4"
               placeholder="stare hasło"
               name="old_password"
               value={old_password}
@@ -131,17 +132,17 @@ const InitialPasswordReset = ({
               required
             />
             <i
-              className="position-absolute eye-icon-large"
+              className="position-absolute eye-icon"
               onClick={() => toggleOldPasswordShow()}
             >
               {<FontAwesomeIcon icon={oldPasswordShown ? faEyeSlash : faEye} />}
             </i>
           </div>
-          <div className="position-relative">
+          <p className="text">Potwierdź nowe hasło</p>
+          <div className="position-relative mb-3 mb-md-4">
             <Input
               id="new_password"
               type={newPasswordShown ? 'text' : 'password'}
-              className="mb-3 mb-md-4"
               placeholder="nowe hasło"
               name="new_password"
               value={new_password}
@@ -152,49 +153,62 @@ const InitialPasswordReset = ({
               required
             />
             <i
-              className="position-absolute eye-icon-large"
+              className="position-absolute eye-icon"
               onClick={() => toggleNewPasswordShow()}
             >
               {<FontAwesomeIcon icon={newPasswordShown ? faEyeSlash : faEye} />}
             </i>
           </div>
           <BlueButton className="mt-5" type="submit">
-            Zatwierdź
+            wyślij
           </BlueButton>
         </form>
       </Wrapper>
 
       {/* PASSWORD VALIDATION: */}
       {changingPassword ? (
-        <Wrapper className="d-flex flex-column justify-content-center align-items-center">
+        <Wrapper className="mt-5 mx-5">
           <div className="text-sm">
-            <p>Twoje nowe hasło musi spełniać następujące wymagania:</p>
-            <p
-              className={`fst-italic ${lowerCase ? 'text-green' : 'text-red'}`}
-            >
-              Małe litery {lowerCase ? <span>&#10004;</span> : null}
-            </p>
-            <p
-              className={`fst-italic ${upperCase ? 'text-green' : 'text-red'}`}
-            >
-              Duże litery {upperCase ? <span>&#10004;</span> : null}
-            </p>
-            <p className={`fst-italic ${numbers ? 'text-green' : 'text-red'}`}>
-              Przynajmniej jedną cyfrę {numbers ? <span>&#10004;</span> : null}
-            </p>
-            <p
-              className={`fst-italic ${
-                specialCharacters ? 'text-green' : 'text-red'
-              }`}
-            >
-              Przynajmniej jeden znak specjalny{' '}
-              {specialCharacters ? <span>&#10004;</span> : null}
-            </p>
-            <p
-              className={`fst-italic ${longEnough ? 'text-green' : 'text-red'}`}
-            >
-              Hasło musi być dłuższe niż 8 znaków{' '}
-              {longEnough ? <span>&#10004;</span> : null}
+            <p className="m-0">
+              Twoje nowe hasło powinno nie być krótsze niż{' '}
+              <span
+                className={`fst-italic ${
+                  longEnough ? 'text-green' : 'text-red'
+                }`}
+              >
+                8 znaków
+              </span>{' '}
+              i zawierać przynajmniej jeden/jedną:{' '}
+              <span
+                className={`fst-italic ${
+                  lowerCase ? 'text-green' : 'text-red'
+                }`}
+              >
+                małą literę
+              </span>
+              {', '}
+              <span
+                className={`fst-italic ${
+                  upperCase ? 'text-green' : 'text-red'
+                }`}
+              >
+                dużą literę
+              </span>
+              {', '}
+              <span
+                className={`fst-italic ${numbers ? 'text-green' : 'text-red'}`}
+              >
+                cyfrę
+              </span>
+              {', '}
+              <span
+                className={`fst-italic ${
+                  specialCharacters ? 'text-green' : 'text-red'
+                }`}
+              >
+                znak specjalny
+              </span>
+              .
             </p>
           </div>
         </Wrapper>
