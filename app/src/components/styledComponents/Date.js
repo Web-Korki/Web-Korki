66b0dc168c496deb1ad74b70e6ca52ed395c8668
Daniel <konnekdaniel@gmail.com>
2021-11-24 16:23:40 +0100

    date height adjustment

--- app/src/components/styledComponents/Date.js
+++ app/src/components/styledComponents/Date.js
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 const Date = styled.input`
   width: 100%;
-  height: max(2em, 3.6rem);
+  padding: 0.2em 0;
   background: #ffffff;
   border: 1px solid #ebeff1;
   box-shadow: 0 0 5px rgba(55, 55, 55, 0.09);
   border-radius: 10px;
   text-align: center;
   color: #195669;
-  font-size: clamp(1.4rem, 2.5vw, 2.2rem);
+  font-size: clamp(1.4rem, 2.5vw, 2rem);
 
   &::placeholder {
     color: #eeeeee;
