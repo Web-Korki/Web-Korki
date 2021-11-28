8fd73ba0c27b819d8216b07ed92b29466665886d
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-11-28 16:50:04 +0100

    propTypes

--- app/src/containers/ActivateAccount.js
+++ app/src/containers/ActivateAccount.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
-import { Wrapper, BlueButton } from '../components/styledComponents/index';
-import { Redirect, useParams } from 'react-router-dom';
+//react
+import React, { useState } from 'react';
+//redux
 import { connect } from 'react-redux';
 import { verify, reset_state } from '../redux/actions/auth';
+//router
+import { Redirect, useParams } from 'react-router-dom';
+//utils
+import { Wrapper, BlueButton } from '../components/styledComponents/index';
+//propTypes
+import PropTypes from 'prop-types';
 
 const ActivateAccount = ({ accountActivated, verify, reset_state }) => {
+  ActivateAccount.propTypes = {
+    accountActivated: PropTypes.bool,
+    verify: PropTypes.func.isRequired,
+    reset_state: PropTypes.func.isRequired,
+  };
+
   const [verified, setVerified] = useState(false);
   const { uid, token } = useParams();
   //uid + token part in regex (?<uid>^[A-Z]{3,})(?<slash>\/)(?<token>.+) matches for eg.NDY/5w0-488f6ea724cf6edd8023
