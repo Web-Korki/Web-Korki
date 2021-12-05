f7b5dfd6410d6f13938a0af8d5af0823a14a8430
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-12-05 13:57:57 +0100

    retrived data from server

--- app/src/containers/substitutions/AwaitingSubstitution/AwaitingSubstitutionData.js
+++ app/src/containers/substitutions/AwaitingSubstitution/AwaitingSubstitutionData.js
@@ -11,60 +11,70 @@ import {
 import PropTypes from 'prop-types';
 
 //dane z reduxa trzeba wyciągnąć komponent wyżej i tutaj podać jako propy, ergo ten komponent wgl nie będzie podłączony do reduxa
-export const AwaitingSubstitutionData = (pendingSubstitutions) => {
+export const AwaitingSubstitutionData = ({ pendingSubstitutionsData }) => {
   AwaitingSubstitutionData.propTypes = {
-    pendingSubstitutions: PropTypes.object,
-  };
-  console.log(pendingSubstitutions); //na etapie wyciągania danych z BE każdy element arrayki trzeba przypisać co jakiejś używalnej nazwy
+    pendingSubstitutionsData: PropTypes.shape({
+      id: PropTypes.number,
+      datetime: PropTypes.string,
+      new_teacher_found: PropTypes.bool,
+      last_topics: PropTypes.string,
+      planned_topics: PropTypes.string,
+      methodology_and_platform: PropTypes.string,
+      old_teacher: PropTypes.number,
+      level: PropTypes.number,
+      subject: PropTypes.number,
+      new_teacher: PropTypes.number,
+    }),
+  }; // what's wrong with propTypes here?
 
   return (
     <div className="col-12">
-      {/* {pendingSubstitutions ? (
-        pendingSubstitutions.map((substitution) => {
-          <Container>
-            <div className="row mb-4">
-              <div className="col-12 col-xl-4 d-flex justify-content-center flex-column mb-4 mb-xl-0">
-                <p className="text">Klasa</p>
-                <TextField>{substitution.level}</TextField>
-              </div>
-              <div className="col-12 col-xl-4 d-flex justify-content-center flex-column mb-4 mb-xl-0">
-                <p className="text">Data</p>
-                <TextField>Data</TextField>
-              </div>
-              <div className="col-12 col-xl-4 d-flex justify-content-center flex-column">
-                <p className="text" for="subject">
-                  Przedmiot
-                </p>
-                <TextField>Przedmiot</TextField>
-              </div>
-            </div>
-            <div className="col d-flex flex-column justify-content-center">
-              <p className="text" for="last-topics">
-                Ostatnio przerabiane zagadnienia
-              </p>
-              <TextField>Dupa na maśle</TextField>
-            </div>
-            <div className="col d-flex flex-column mt-4 justify-content-center">
-              <p className="text" for="planned-topics">
-                Planowane zagadnienia na lekcję
-              </p>
-              <TextField>Masło w dupie</TextField>
-            </div>
-            <div className="col d-flex flex-column mt-4 justify-content-center">
-              <p className="text" for="teaching-methodology">
-                Metodyka nauczania oraz platforma
-              </p>
-              <TextField>Teams ssie, zoom mniej</TextField>
-            </div>
-            <div className="col d-flex mt-4 justify-content-between">
-              <GreyButton>odrzuć</GreyButton>
-              <BlueButton>akceptuj</BlueButton>
-            </div>
-          </Container>;
-        })
-      ) : (
-        <div>Żaden z nauczycieli nie oczekuje zastępstwa.</div>
-      )} */}
+      {pendingSubstitutionsData
+        ? pendingSubstitutionsData.map((substitution) => {
+            return (
+              <Container className="mb-5">
+                <div className="row mb-4">
+                  <div className="col-12 col-xl-4 d-flex justify-content-center flex-column mb-4 mb-xl-0">
+                    <p className="text">Klasa</p>
+                    <TextField>{substitution.level}</TextField>
+                  </div>
+                  <div className="col-12 col-xl-4 d-flex justify-content-center flex-column mb-4 mb-xl-0">
+                    <p className="text">Data</p>
+                    <TextField>Data</TextField>
+                  </div>
+                  <div className="col-12 col-xl-4 d-flex justify-content-center flex-column">
+                    <p className="text" for="subject">
+                      Przedmiot
+                    </p>
+                    <TextField>Przedmiot</TextField>
+                  </div>
+                </div>
+                <div className="col d-flex flex-column justify-content-center">
+                  <p className="text" for="last-topics">
+                    Ostatnio przerabiane zagadnienia
+                  </p>
+                  <TextField>Dupa na maśle</TextField>
+                </div>
+                <div className="col d-flex flex-column mt-4 justify-content-center">
+                  <p className="text" for="planned-topics">
+                    Planowane zagadnienia na lekcję
+                  </p>
+                  <TextField>Masło w dupie</TextField>
+                </div>
+                <div className="col d-flex flex-column mt-4 justify-content-center">
+                  <p className="text" for="teaching-methodology">
+                    Metodyka nauczania oraz platforma
+                  </p>
+                  <TextField>Teams ssie, zoom mniej</TextField>
+                </div>
+                <div className="col d-flex mt-4 justify-content-between">
+                  <GreyButton>odrzuć</GreyButton>
+                  <BlueButton>akceptuj</BlueButton>
+                </div>
+              </Container>
+            );
+          })
+        : null}
     </div>
   );
 };
--- app/src/containers/substitutions/AwaitingSubstitution/AwaitingSubstitutions.js
+++ app/src/containers/substitutions/AwaitingSubstitution/AwaitingSubstitutions.js
@@ -12,10 +12,23 @@ import PropTypes from 'prop-types';
 
 const AwaitingSubstitutions = ({
   get_pending_substitutions,
-  pending_substitutions,
+  pendingSubstitutionsCount,
+  pendingSubstitutionsData,
 }) => {
   AwaitingSubstitutions.propTypes = {
-    pending_substitutions: PropTypes.object,
+    pendingSubstitutionsCount: PropTypes.number,
+    pendingSubstitutionsData: PropTypes.shape({
+      id: PropTypes.number,
+      datetime: PropTypes.string,
+      new_teacher_found: PropTypes.bool,
+      last_topics: PropTypes.string,
+      planned_topics: PropTypes.string,
+      methodology_and_platform: PropTypes.string,
+      old_teacher: PropTypes.number,
+      level: PropTypes.number,
+      subject: PropTypes.number,
+      new_teacher: PropTypes.number,
+    }),
   };
 
   useEffect(() => {
@@ -28,12 +41,16 @@ const AwaitingSubstitutions = ({
         <div className="d-flex justify-content-center flex-lg-row flex-column align-items-center mb-5">
           <div className="d-flex justify-content-between align-items-center mb-4 mb-lg-0">
             <BackButton />
-            <h1 className="title">Oczekujące zastępstwa: {pending_substitutions ? pending_substitutions.length : null}</h1>
+            <h1 className="title">
+              Oczekujące zastępstwa: {pendingSubstitutionsCount}
+            </h1>
           </div>
         </div>
         <div className="limiter row g-4">
           {/* mapka =>  */}
-          <AwaitingSubstitutionData pendingSubstitutions={pending_substitutions}/>
+          <AwaitingSubstitutionData
+            pendingSubstitutionsData={pendingSubstitutionsData}
+          />
         </div>
       </div>
     </div>
@@ -41,7 +58,8 @@ const AwaitingSubstitutions = ({
 };
 
 const mapStateToProps = (state) => ({
-  pending_substitutions: state.substitutions?.pending_substitutions,
+  pendingSubstitutionsCount: state.substitutions?.pending_substitutions?.count,
+  pendingSubstitutionsData: state.substitutions?.pending_substitutions?.results,
 });
 
 export default connect(mapStateToProps, { get_pending_substitutions })(
