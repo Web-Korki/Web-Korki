f6ff1f8825b5e21511d933a4170b3f4264b82797
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-12-11 22:18:32 +0100

    retriving data from the submit substitution form

--- app/src/containers/substitutions/SubmitSubstitution/SubmitSubstitution.js
+++ app/src/containers/substitutions/SubmitSubstitution/SubmitSubstitution.js
@@ -1,154 +1,30 @@
-import React, { useEffect, useState } from 'react';
+//react
+import React from 'react';
+//redux
 import { connect } from 'react-redux';
-import {
-  Date,
-  Textarea,
-  BlueButton,
-  Wrapper,
-} from '../../../components/styledComponents/index';
-import { BackButton } from '../../../components/buttons/BackButton';
 import {
   get_levels,
   get_subjects,
 } from '../../../redux/actions/substitutionForm';
-//propTypes
+// utils
+import SubmitSubstitutionData from './SubmitSubstitutionData';
+// propTypes
 import PropTypes from 'prop-types';
-import Select from 'react-select';
-import Theme from '../../../components/data/Theme';
 
-const SubmitSubstitution = ({ get_levels, get_subjects, formSelectData }) => {
+const SubmitSubstitution = ({ formSelectData, get_levels, get_subjects }) => {
   SubmitSubstitution.propTypes = {
+    get_levels: PropTypes.func.isRequired,
+    get_subjects: PropTypes.func.isRequired,
     formSelectData: PropTypes.object.isRequired,
   };
 
-  useEffect(() => {
-    get_levels();
-    get_subjects();
-  }, []);
-
-  const [formData, setFormData] = useState({
-    level: '',
-    datetime: '',
-    subject: '',
-    last_topics: '',
-    planned_topics: '',
-    methodology_and_platform: '',
-  });
-
-  const {
-    level,
-    datetime,
-    subject,
-    last_topics,
-    planned_topics,
-    methodology_and_platform,
-  } = formData;
-
-  const onChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const defaultFormSelectData = [{ value: '', label: '' }];
-
   return (
-    <div className="min-h-100 py-5 py-xl-0 container-fluid container-xl d-flex flex-column justify-content-center align-items-center">
-      <Wrapper>
-        <div class="d-flex mb-2 mb-xl-5">
-          <BackButton />
-          <h1 className="title">Formularz zgłaszania zastępstwa</h1>
-        </div>
-        <form className="w-100 d-flex flex-column p-4 mb-3">
-          <div className="row mb-4">
-            <div className="col-12 col-xl-4 d-flex justify-content-center flex-column mb-4 mb-xl-0">
-              <label className="text" for="class">
-                Klasa
-              </label>
-              <Select
-                className="text-select"
-                placeholder="Wybierz klasę"
-                theme={Theme}
-                name="level"
-                options={
-                  formSelectData.levels
-                    ? formSelectData.levels.map((e) => {
-                        return {
-                          value: e.id,
-                          label: e.name,
-                        };
-                      })
-                    : defaultFormSelectData
-                }
-              />
-            </div>
-            <div className="col-12 col-xl-4 d-flex justify-content-center flex-column mb-4 mb-xl-0">
-              <label for="date" className="text">
-                Data
-              </label>
-              <div className="d-flex justify-content-center">
-                <Date
-                  className="d-flex justify-content-center"
-                  type="datetime-local"
-                  id="date"
-                  required
-                />
-              </div>
-            </div>
-            <div className="col-12 col-xl-4 d-flex justify-content-center flex-column">
-              <label className="text" for="subject">
-                Przedmiot
-              </label>
-              <Select
-                className="text-select"
-                placeholder="Wybierz przedmiot"
-                theme={Theme}
-                name="subject"
-                options={
-                  formSelectData.subjects
-                    ? formSelectData.subjects.map((e) => {
-                        return {
-                          value: e.id,
-                          label: e.name,
-                        };
-                      })
-                    : defaultFormSelectData
-                }
-              />
-            </div>
-          </div>
-          <div className="col d-flex flex-column justify-content-center">
-            <label className="text" for="last_topics">
-              Ostatnio przerabiane zagadnienia
-            </label>
-            <Textarea
-              className="align-self-center ps-4"
-              name="last_topics"
-              placeholder="Temat ostatniej lekcji to..."
-            ></Textarea>
-          </div>
-          <div className="col d-flex flex-column mt-4 justify-content-center">
-            <label className="text" for="planned_topics">
-              Planowane zagadnienia na lekcję
-            </label>
-            <Textarea
-              className="align-self-center ps-4"
-              name="planned_topics"
-              placeholder="Temat przyszłej lekcji to..."
-            ></Textarea>
-          </div>
-          <div className="col d-flex flex-column mt-4 justify-content-center">
-            <label className="text" for="methodology_and_platform">
-              Metodyka nauczania oraz platforma
-            </label>
-            <Textarea
-              className="align-self-center ps-4"
-              name="methodology_and_platform"
-              placeholder="Z uczniem pracujemy korzystając z..."
-            ></Textarea>
-          </div>
-        </form>
-        <BlueButton>zgłoś zastępstwo</BlueButton>
-      </Wrapper>
-    </div>
+    <SubmitSubstitutionData
+      getLevels={get_levels}
+      getSubjects={get_subjects}
+      levels={formSelectData.levels}
+      subjects={formSelectData.subjects}
+    />
   );
 };
 
