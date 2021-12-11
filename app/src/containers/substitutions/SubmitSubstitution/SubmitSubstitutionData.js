545a49456fd5f591f59c7553c194e1fb14b109d3
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-12-11 23:06:54 +0100

    syntax issues

---
+++ app/src/containers/substitutions/SubmitSubstitution/SubmitSubstitutionData.js
@@ -0,0 +1,189 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Date,
+  Textarea,
+  BlueButton,
+  Wrapper,
+} from '../../../components/styledComponents/index';
+import { BackButton } from '../../../components/buttons/BackButton';
+//propTypes
+import PropTypes from 'prop-types';
+import Select from 'react-select';
+import Theme from '../../../components/data/Theme';
+
+const SubmitSubstitutionData = ({
+  levels,
+  subjects,
+  getLevels,
+  getSubjects,
+}) => {
+  SubmitSubstitutionData.propTypes = {
+    getLevels: PropTypes.func.isRequired,
+    getSubjects: PropTypes.func.isRequired,
+    formSelectData: PropTypes.object.isRequired,
+  };
+
+  useEffect(() => {
+    getLevels();
+    getSubjects();
+  }, []);
+
+  const [formData, setFormData] = useState({
+    level: '',
+    datetime: '',
+    subject: '',
+    last_topics: '',
+    planned_topics: '',
+    methodology_and_platform: '',
+  });
+
+  const [selectedLevel, setSelectedLevel] = useState('');
+  const [selectedSubject, setSelectedSubject] = useState('');
+
+  // {...formData, level: selectedLevel, subject: selectedSubject}
+
+  const {
+    level,
+    datetime,
+    subject,
+    last_topics,
+    planned_topics,
+    methodology_and_platform,
+  } = formData;
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      level: selectedLevel,
+      subject: selectedSubject,
+      [e.target.name]: e.target.value,
+    });
+  }; // przechowuje wartość selectedLevel i selectedSubject ale z opóźnieniem do czasu aktualizacji innych pól. Czy należałoby dać e.name: e.label??? coś nie tak ze składnią
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // action to create substitution;
+  };
+
+  // console.log('levels', levels, 'subjects', subjects);
+  console.log(formData);
+
+  const defaultFormSelectData = [{ value: '', label: '' }];
+
+  return (
+    <div className="min-h-100 py-5 py-xl-0 container-fluid container-xl d-flex flex-column justify-content-center align-items-center">
+      <Wrapper>
+        <div class="d-flex mb-2 mb-xl-5">
+          <BackButton />
+          <h1 className="title">Formularz zgłaszania zastępstwa</h1>
+        </div>
+        <form className="w-100 d-flex flex-column p-4 mb-3">
+          <div className="row mb-4">
+            <div className="col-12 col-xl-4 d-flex justify-content-center flex-column mb-4 mb-xl-0">
+              <label className="text" for="class">
+                Klasa
+              </label>
+              <Select
+                className="text-select"
+                placeholder="Wybierz klasę"
+                theme={Theme}
+                name="level"
+                options={
+                  levels
+                    ? levels.map((e) => {
+                        return {
+                          value: e.id,
+                          label: e.name,
+                        };
+                      })
+                    : defaultFormSelectData
+                }
+                onChange={(e) => {
+                  setSelectedLevel(e.label);
+                }}
+              />
+            </div>
+            <div className="col-12 col-xl-4 d-flex justify-content-center flex-column mb-4 mb-xl-0">
+              <label for="date" className="text">
+                Data
+              </label>
+              <div className="d-flex justify-content-center">
+                <Date
+                  className="d-flex justify-content-center"
+                  type="datetime-local"
+                  id="date"
+                  value={datetime}
+                  onChange={(e) => onChange(e)}
+                  required
+                />
+              </div>
+            </div>
+            <div className="col-12 col-xl-4 d-flex justify-content-center flex-column">
+              <label className="text" for="subject">
+                Przedmiot
+              </label>
+              <Select
+                className="text-select"
+                placeholder="Wybierz przedmiot"
+                theme={Theme}
+                name="subject"
+                options={
+                  subjects
+                    ? subjects.map((e) => {
+                        return {
+                          value: e.id,
+                          label: e.name,
+                        };
+                      })
+                    : defaultFormSelectData
+                }
+                onChange={(e) => {
+                  setSelectedSubject(e.label);
+                }}
+              />
+            </div>
+          </div>
+          <div className="col d-flex flex-column justify-content-center">
+            <label className="text" for="last_topics">
+              Ostatnio przerabiane zagadnienia
+            </label>
+            <Textarea
+              className="align-self-center ps-4"
+              name="last_topics"
+              value={last_topics}
+              onChange={(e) => onChange(e)}
+              placeholder="Temat ostatniej lekcji to..."
+            ></Textarea>
+          </div>
+          <div className="col d-flex flex-column mt-4 justify-content-center">
+            <label className="text" for="planned_topics">
+              Planowane zagadnienia na lekcję
+            </label>
+            <Textarea
+              className="align-self-center ps-4"
+              name="planned_topics"
+              value={planned_topics}
+              onChange={(e) => onChange(e)}
+              placeholder="Temat przyszłej lekcji to..."
+            ></Textarea>
+          </div>
+          <div className="col d-flex flex-column mt-4 justify-content-center">
+            <label className="text" for="methodology_and_platform">
+              Metodyka nauczania oraz platforma
+            </label>
+            <Textarea
+              className="align-self-center ps-4"
+              name="methodology_and_platform"
+              value={methodology_and_platform}
+              onChange={(e) => onChange(e)}
+              placeholder="Z uczniem pracujemy korzystając z..."
+            ></Textarea>
+          </div>
+        </form>
+        <BlueButton>zgłoś zastępstwo</BlueButton>
+      </Wrapper>
+    </div>
+  );
+};
+
+export default SubmitSubstitutionData;
