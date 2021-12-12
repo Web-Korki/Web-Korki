bbc36ce9bcc9821febf8e9b3ed3bdac7c1970ef7
Daniel Konnek <konnekdaniel@gmail.com>
2021-12-12 15:57:08 +0100

    added react-spring for animations, removed CookieOverlay component and replaced it with div, handled unmounting component if cookie is present

--- app/src/containers/CookiePopup.js
+++ app/src/containers/CookiePopup.js
@@ -1,53 +1,67 @@
 // import Cookies from 'js-cookie';
-import {
-  BlueButton,
-  CookieContainer,
-  CookieOverlay,
-} from '../components/styledComponents';
+import { BlueButton, CookieContainer } from '../components/styledComponents';
 import { CookieIcon } from '../components/layout/CookieIcon';
 import Cookies from 'js-cookie';
 import { useState } from 'react';
+import { useTransition, animated } from 'react-spring';
+import { unmountComponentAtNode } from 'react-dom';
 
 const CookiePopup = () => {
-  const [cookie, setCookie] = useState(Cookies.get('wk_consent') ? true : null);
-  const [show, setShow] = useState(false);
+  const [cookie, setCookie] = useState(
+    Cookies.get('wk_consent') ? true : false
+  );
+  const [mount, setMount] = useState(false);
 
   setTimeout(() => {
-    setShow(true);
-  }, 2000);
+    setMount(true);
+  }, 1500);
 
-  return !cookie && show ? (
-    <CookieOverlay>
-      <CookieContainer>
-        <div className="row mb-4">
-          <CookieIcon />
-          <h1 className="col align-self-center title">
-            Stosujemy politykę Cookies
-          </h1>
-        </div>
-        <div className="row">
-          <p className="text col col-lg-10 px-5 px-lg-0 mx-auto mb-4">
-            [Miejsce na tekst o ciasteczkach itd. Jak ma być polityka
-            prywatności, to daj znać]. Ciasteczko aktualnie wygasa po 14 dniach
-            (chyba, że jakiś ad-blocker usunie go wcześniej/po zakończeniu
-            sesji)
-          </p>
-        </div>
-        <div className="row">
-          <BlueButton
-            className="col-auto me-auto me-lg-0 ms-auto"
-            onClick={() =>
-              setCookie(
-                Cookies.set('wk_consent', true, { expires: 14, path: '/' })
-              )
-            }
-          >
-            rozumiem
-          </BlueButton>
-        </div>
-      </CookieContainer>
-    </CookieOverlay>
-  ) : null;
+  const transition = useTransition(!cookie && mount, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
+  const handleClick = () => {
+    setCookie(
+      Cookies.set('wk_consent', true, {
+        expires: 14,
+        path: '/',
+      })
+    );
+    unmountComponentAtNode(document.getElementById('cookie-pop-up'));
+  };
+
+  return transition((style, item) =>
+    item ? (
+      <animated.div className="cookie-overlay" id="cookie-pop-up" style={style}>
+        <CookieContainer>
+          <div className="row mb-4">
+            <CookieIcon />
+            <h1 className="col align-self-center title">
+              Stosujemy politykę Cookies
+            </h1>
+          </div>
+          <div className="row">
+            <p className="text col col-lg-10 px-5 px-lg-0 mx-auto mb-4">
+              [Miejsce na tekst o ciasteczkach itd. Jak ma być polityka
+              prywatności, to się doda]. Ciasteczko aktualnie wygasa po 14
+              dniach (chyba, że jakiś ad-blocker usunie go wcześniej/po
+              zakończeniu sesji)
+            </p>
+          </div>
+          <div className="row">
+            <BlueButton
+              className="col-auto me-auto me-lg-0 ms-auto"
+              onClick={handleClick}
+            >
+              rozumiem
+            </BlueButton>
+          </div>
+        </CookieContainer>
+      </animated.div>
+    ) : null
+  );
 };
 
 export default CookiePopup;
