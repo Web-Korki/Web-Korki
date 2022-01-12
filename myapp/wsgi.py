deedcb92eb2daaba01274f5d1dca0bcd1931ab97
Tomasz Jastrzębski <tomasz.jastrzebski95@gmail.com>
2022-01-12 12:45:47 +0100

    heroku push

--- myapp/wsgi.py
+++ myapp/wsgi.py
@@ -8,18 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-this_file = "www/venv/bin/activate_this.py"
-exec(open(this_file).read(), {"__file__": this_file})
-
-# add your project directory to the sys.path
-project_home = u"/home/nujgoiz/myapp"
-if project_home not in sys.path:
-    sys.path.append(project_home)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings")
 
 application = get_wsgi_application()
