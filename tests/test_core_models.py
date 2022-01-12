f980da83bc686c1656b8766b1898a39f5eefa0af
Tomasz Jastrzębski <tomasz.jastrzebski95@gmail.com>
2022-01-12 16:58:09 +0100

    correct tests

--- tests/test_core_models.py
+++ tests/test_core_models.py
@@ -5,14 +5,14 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "myapp.settings"
 django.setup()
 
 import pytest
-from django.urls import reverse
+from django.urls import reverse_lazy
 from tests.utils import model_view_set_test
 from backend.models import Level, Subject, Student, House
 from datetime import date, timedelta
 
-houses_list_url = reverse("houses-list")
-lessons_list_url = reverse("lessons-list")
-students_list_url = reverse("students-list")
+houses_list_url = reverse_lazy("houses-list")
+lessons_list_url = reverse_lazy("lessons-list")
+students_list_url = reverse_lazy("students-list")
 
 
 @pytest.mark.django_db
