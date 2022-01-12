26b90f7bbf2d693bb42b797be1f52f49becb4ca3
Tomasz Jastrzębski <tomasz.jastrzebski95@gmail.com>
2022-01-12 14:38:50 +0100

    heroku push

--- myapp/settings.py
+++ myapp/settings.py
@@ -258,7 +258,7 @@ CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://lessons-planer.herokuapp.com/"
+    "https://lessons-planer.herokuapp.com"
 ]
 APPEND_SLASH = False
 
