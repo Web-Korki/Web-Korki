4bc9623b1ffd3a40dd80eba391b649940c45d442
Tomasz Jastrzębski <tomasz.jastrzebski95@gmail.com>
2022-01-12 14:44:18 +0100

    heroku push

--- myapp/urls.py
+++ myapp/urls.py
@@ -49,5 +49,6 @@ urlpatterns = [
         ChangePasswordAfterRegister.as_view({"patch": "update"}),
     ),
     path(r"redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    re_path(r"^.*", index, name="index"),
     # All urls not specified in backend will be handled by react frontend
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
